05803ba3207f6636ccafe8c623c401028516be7e
Matthias Dröge <Zelophobian@users.noreply.github.com>
2016-02-22 22:41:37 +0100

    Update Human.java

--- src/races/Human.java
+++ src/races/Human.java
@@ -1,26 +1,14 @@
 package races;
 public class Human extends Race
 {
-  private String name;
-  private double[] BonusStats;
-  // [0] strength
-  // [1] dexterity
-  // [2] Constitution
-  // [3] wisdom
-  // [4] inteligent
-  // [5] charisma
-  // [6] movement
-  // [7] health (calculated)
-  
   public Human()
   {
-	name = this.getClass().getSimpleName();
-	super.setName(name);
-	BonusStats = new double[8];
-	BonusStats = super.getStatsBonus();
+	setName(this.getClass().getSimpleName());
+	double[] BonusStats = super.getStatsBonus();
 	for(int i = 0; i < BonusStats.length; i++)
 	{
 	  BonusStats[i] += 1;
 	}
+	setStatsBonus(BonusStats);
   }
 }

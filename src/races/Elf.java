b7fc2f7b5fa0ec23e732393d7bf0e0e3a65bbd45
Matthias Dröge <Zelophobian@users.noreply.github.com>
2016-02-22 22:41:16 +0100

    Update Elf.java

--- src/races/Elf.java
+++ src/races/Elf.java
@@ -1,26 +1,12 @@
 package races;
-
 public class Elf extends Race
 {
-  private String name;
-  private double[] BonusStats;
-  // [0] strength
-  // [1] dexterity
-  // [2] Constitution
-  // [3] wisdom
-  // [4] inteligent
-  // [5] charisma
-  // [6] movement
-  // [7] health (calculated)
-  
   public Elf()
   {
-	name = this.getClass().getSimpleName();
-	super.setName(name);
-	BonusStats = new double[8];
-	BonusStats = super.getStatsBonus();
+	setName(this.getClass().getSimpleName());
+	double[] BonusStats = super.getStatsBonus();
     BonusStats[1] += 2;
     BonusStats[3] += 2;
-	
+	setStatsBonus(BonusStats);
   }
 }

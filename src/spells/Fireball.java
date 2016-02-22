94e37b0b8bb314661bea7b9a04d9a970de80fb50
Matthias Dröge <Zelophobian@users.noreply.github.com>
2016-02-22 22:42:36 +0100

    Update Fireball.java

--- src/spells/Fireball.java
+++ src/spells/Fireball.java
@@ -2,5 +2,12 @@ package spells;
 
 public class Fireball extends Spell
 {
-  
+  public Fireball()
+  {
+	setName(this.getClass().getSimpleName());
+	setCastingTime(1);
+	setSpellEffect("An explosion of fire, with an areal effect.");
+	setDamageDie(6);
+	setDieCount(3);
+  }
 }

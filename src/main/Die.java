2e798461c0dbb7a714eb8fe47275636122ec0ac9
Zelophobian <zelophobian@gmail.com>
2016-02-26 20:06:17 +0100

    Get this up to date.

--- src/main/Die.java
+++ src/main/Die.java
@@ -9,10 +9,19 @@ public class Die
 	Random r = new Random();
 	for(int i = 1; i <= (int) dieCount; i++)
 	{
-	  output += r.nextInt((int)nSides);
 	  // Weil nextInt(int) von 0 bis nSides-1 geht.
-	  output += 1;
+	  output += r.nextInt((int)nSides) + 1;
 	}
 	return (double) output;
+  }
+  
+  public static double rollDie_recursively(double nSides, double dieCount)
+  {
+	int output = 0;
+	Random r = new Random();
+	output += r.nextInt((int)nSides) + 1;
+	dieCount -= 1.0;
+	output += (dieCount > 0) ? rollDie_recursively(nSides, dieCount) : 0.0;
+	return output;
   }
 }

28bb5cc6453cdc8e86e38d5f30c61938c7b91606
Steffen Haas <s.haas@opwoco.de>
2016-04-29 15:03:05 +0200

    refactoring of item

--- src/BaseStats/Item.java
+++ src/BaseStats/Item.java
@@ -9,6 +9,53 @@ package BaseStats;
  *
  * @author steffen
  */
-public class BaseObject {
+public class Item {
     
+    
+    private Size size;
+    private double weigth;
+    
+    public Item() {
+        this.size = new Size();
+        this.weigth = 1.0;
+    }
+    
+    public Item(double weight, double length, double width) {
+        this.size = new Size();
+        this.size.length = length;
+        this.size.width = width;
+        this.weigth = weight;
+                
+                
+    }
+
+    /**
+     * @return the size
+     */
+    public Size getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    /**
+     * @return the weigth
+     */
+    public double getWeigth() {
+        return weigth;
+    }
+
+    /**
+     * @param weigth the weigth to set
+     */
+    public void setWeigth(double weigth) {
+        this.weigth = weigth;
+    }
 }
+
+

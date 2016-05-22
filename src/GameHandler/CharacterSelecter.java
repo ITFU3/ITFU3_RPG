77daeb3a73ed3c0677b8188071a4a448619803ba
Zelophobian <zelophobian@gmail.com>
2016-05-22 10:51:28 +0200

    Character_Selecter implemented (taken from main an put to the Handler)

---
+++ src/GameHandler/CharacterSelecter.java
@@ -0,0 +1,88 @@
+package GameHandler;
+import armor.*;
+import characters.*;
+import classes.*;
+import races.*;
+import spells.*;
+import weapons.*;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Matthias
+ */
+public class CharacterSelecter
+{
+  public static PlayerCharacter selectCharacter()
+  {
+    // for testing some chars in an array
+    // in this way it is not so much overhead
+    PlayerCharacter[] players = getDemoChars();
+    
+    // Preview
+    for(int i=0; i< players.length;i++)
+    {
+      System.out.println("Player " + (i+1) + ":");
+      System.out.println(
+          "Player name: " + players[i].getName() + "\n" +
+          "Player class: " + players[i].getpClass().getName() + "\n" +
+          "Player race: " + players[i].getpRace().getName() + "\n"
+          );
+    }
+
+    System.out.println("Please select Player:");
+    
+    // The Selecting
+    Scanner input = new Scanner(System.in);
+    int choice = (input.nextInt()-1);
+    System.out.println("\n");
+    
+    // Final output
+    return players[choice];
+  }
+  
+  public static PlayerCharacter[] getDemoChars()
+  {
+    PlayerCharacter player1 = new PlayerCharacter(
+                                      "Zelo",
+                                      'm',
+                                      new Fighter(), 
+                                      new Human()
+                                    );
+    player1.addWeapon(new LongSword("Excalibur", 10, 1, 1));
+    player1.addArmor(new Plate());
+    
+    PlayerCharacter player2 = new PlayerCharacter(
+                                      "Gwen",
+                                      'f',
+                                      new Cleric(), 
+                                      new Dwarf()
+                                    );
+    player2.addWeapon(new Mace("Glower", 8, 1, 1));
+    player2.addArmor(new ChainMail());
+    player2.getpClass().getMyBook().addSpell(new HealingWord());
+    
+    PlayerCharacter player3 = new PlayerCharacter(
+                                      "Vahlran",
+                                      'm',
+                                      new Ranger(), 
+                                      new Elf()
+                                    );
+    player3.addWeapon(new ShortBow("Feather", 6, 2, 10));
+    player3.addArmor(new Leather());
+    
+    PlayerCharacter player4 = new PlayerCharacter(
+                                "Simon",
+                                'm',
+                                new Wizzard(), 
+                                new Human()
+                              );
+    player4.addWeapon(new Weapon());
+    player4.addArmor(new Cloth());
+    player4.getpClass().getMyBook().addSpell(new Fireball());
+    
+    PlayerCharacter[] output = {player1, player2, player3, player4};
+    return output;
+  }
+}
--- src/main/Demos.java
+++ src/main/Demos.java
@@ -15,49 +15,6 @@ import java.util.Scanner;
 
 public class Demos 
 {
-  public static PlayerCharacter[] getDemoChars()
-  {
-    PlayerCharacter player1 = new PlayerCharacter(
-                                      "Zelo",
-                                      'm',
-                                      new Fighter(), 
-                                      new Human()
-                                    );
-    player1.addWeapon(new LongSword("Excalibur", 10, 1, 1));
-    player1.addArmor(new Plate());
-    
-    PlayerCharacter player2 = new PlayerCharacter(
-                                      "Gwen",
-                                      'f',
-                                      new Cleric(), 
-                                      new Dwarf()
-                                    );
-    player2.addWeapon(new Mace("Glower", 8, 1, 1));
-    player2.addArmor(new ChainMail());
-    player2.getpClass().getMyBook().addSpell(new HealingWord());
-    
-    PlayerCharacter player3 = new PlayerCharacter(
-                                      "Vahlran",
-                                      'm',
-                                      new Ranger(), 
-                                      new Elf()
-                                    );
-    player3.addWeapon(new ShortBow("Feather", 6, 2, 10));
-    player3.addArmor(new Leather());
-    
-    PlayerCharacter player4 = new PlayerCharacter(
-                                "Simon",
-                                'm',
-                                new Wizzard(), 
-                                new Human()
-                              );
-    player4.addWeapon(new Weapon());
-    player4.addArmor(new Cloth());
-    player4.getpClass().getMyBook().addSpell(new Fireball());
-    
-    PlayerCharacter[] output = {player1, player2, player3, player4};
-    return output;
-  }
   public static MonsterCharacter[] getDemoMonster()
   {
     MonsterCharacter monster = new MonsterCharacter();

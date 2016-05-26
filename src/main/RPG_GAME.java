ca181fade9779112aab7d7b20025e1756c3e3529
Zelophobian <zelophobian@gmail.com>
2016-05-26 14:05:10 +0200

    sorry should not be committed.

--- src/main/RPG_GAME.java
+++ src/main/RPG_GAME.java
@@ -7,20 +7,19 @@ public class RPG_GAME
 {
   public static void main(String[] args)
   {
-    BattleHandler bH = new BattleHandler();
-//    utilities.InputHandler.openInputReader();
-//    
-//    // can be used for ramdom monster selection
-//    MonsterCharacter[] monsters = Demos.getDemoMonster();
-//       
-//    // Selecting Player
-//    PlayerCharacter selectedCharacter = CharacterSelecter.selectCharacter();
-//    
-//	  selectedCharacter.showCharInfo();
-//    
-//    // Starting the Game
-//    Demos.alphaVersion(selectedCharacter, monsters[1]);
-//    
-//    utilities.InputHandler.closeInputReader();
+    utilities.InputHandler.openInputReader();
+    
+    // can be used for ramdom monster selection
+    MonsterCharacter[] monsters = Demos.getDemoMonster();
+       
+    // Selecting Player
+    PlayerCharacter selectedCharacter = CharacterSelecter.selectCharacter();
+    
+	  selectedCharacter.showCharInfo();
+    
+    // Starting the Game
+    Demos.alphaVersion(selectedCharacter, monsters[1]);
+    
+    utilities.InputHandler.closeInputReader();
   }
 }

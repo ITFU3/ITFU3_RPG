17bff65739e4077b5171f5a184531f99f578158b
Matthias Dröge <Zelophobian@gmail.com>
2017-06-10 22:45:06 +0200

    playing with the Frames

--- src/gui/popups/HighScoreFrame.java
+++ src/gui/popups/HighScoreFrame.java
@@ -1,18 +1,9 @@
 package gui.popups;
 
-import gui.SelectionFrame;
-
-import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.*;
 import java.util.ArrayList;
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.RowSorter;
-import javax.swing.SortOrder;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
-import javax.swing.table.TableRowSorter;
+import javax.swing.*;
+import javax.swing.table.*;
 
 /**
  *
@@ -20,45 +11,67 @@ import javax.swing.table.TableRowSorter;
  */
 public class HighScoreFrame extends JFrame
 {
-    private JScrollPane scrollPane = new JScrollPane();
-    private JTable table = new JTable();
+    private final JScrollPane scrollPane;
+    private final JTable table;
     
-    public HighScoreFrame() {
-        this.setLayout(new GridLayout(0, 1));
-        this.initList();
-        this.configInternals();
+    public HighScoreFrame()
+    {
+        this.scrollPane = new JScrollPane();
+        this.table = new JTable();
         
-        TableRowSorter<TableModel> sorter = new TableRowSorter(table.getModel());
-        table.setRowSorter(sorter);
-        ArrayList<RowSorter.SortKey> sortKeys = new ArrayList<>(25);
-        sortKeys.add(new RowSorter.SortKey(2, SortOrder.DESCENDING));
-        sorter.setSortKeys(sortKeys);
-        table.setEnabled(false);
-        table.setFocusable(false);
+        this.initFrame();
+        this.addTableModel();
+        this.addSorter();
     }
 
-    private void initList() {
+    /**
+     * std. frame preps
+     */
+    private void initFrame()
+    {
+        this.setName("HighScore");
+        this.setTitle(this.getName());
+        
+        this.setLayout(new GridLayout(0, 1));
+        this.setSize(new Dimension(400, 150));
+        this.setResizable(false);
+        
+        this.table.setEnabled(false);
+        this.table.setFocusable(false);
+        
+        this.scrollPane.setEnabled(false);
+        this.scrollPane.setFocusable(false);
+        
+        this.add(this.scrollPane);
+    }
+    
+    /**
+     * adding the model to the table
+     */
+    private void addTableModel()
+    {
         String[] colName = {"Name", "level", "Score"};
         ArrayList<String[]> scoreList = fileHandler.DataHandler.readHighscoreList();
         String[][] output = new String[scoreList.size()][3];
-        
-        for(int i = 0; i < scoreList.size(); i++) {
+        for(int i = 0; i < scoreList.size(); i++)
+        {
             String[] temp = { scoreList.get(i)[0], scoreList.get(i)[1], scoreList.get(i)[2] };
             output[i] = temp;
         }
         DefaultTableModel scoreTableModel = new DefaultTableModel(output, colName);
-
         this.table.setModel(scoreTableModel);
-        this.scrollPane.setViewportView(table);
-        this.add(this.scrollPane);
+        this.scrollPane.setViewportView(this.table);
     }
 
-    private void configInternals() {
-        Dimension d = new Dimension(400, 150);
-        this.scrollPane.setSize(d);
-        this.setSize(d);
-        this.setResizable(false);
-        this.setName("HighScore");
-        this.setTitle(this.getName());
+    /**
+     * adding a sorter
+     */
+    private void addSorter()
+    {
+        TableRowSorter<TableModel> sorter = new TableRowSorter(this.table.getModel());
+        this.table.setRowSorter(sorter);
+        ArrayList<RowSorter.SortKey> sortKeys = new ArrayList(25);
+        sortKeys.add(new RowSorter.SortKey(2, SortOrder.DESCENDING));
+        sorter.setSortKeys(sortKeys);
     }
 }
--- src/gui/popups/SpellbookFrame.java
+++ src/gui/popups/SpellbookFrame.java
@@ -1,15 +1,13 @@
 package gui.popups;
 
-import character.MonsterCharacter;
-import character.PlayerCharacter;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.event.*;
 
+import character.*;
 import character.item.spells.*;
 import gameHandler.BattleHandler;
 import main.Game;
-import main.Map;
 
 /**
  *
@@ -17,38 +15,97 @@ import main.Map;
  */
 public class SpellbookFrame extends JFrame
 {
-    private JScrollPane scrollPane = new JScrollPane();
-    private JList spellList = new JList();
-    private JTextArea spellInfoArea = new JTextArea();
-    private JButton castSpellButton = new JButton();
-    
-    private String selectedSpell = "";
+    private final JScrollPane scrollPane;
+    private final JList spellList;
+    private final JTextArea spellInfoArea;
+    private final JButton castSpellButton;
+    private String selectedSpell;
     
     public SpellbookFrame()
     {
-        this.setLayout(new GridLayout(0, 1));
-        this.initList();
-        this.configInternals();
+        this.scrollPane = new JScrollPane();
+        this.spellList = new JList();
+        this.spellInfoArea = new JTextArea();
+        this.castSpellButton = new JButton();
+        this.selectedSpell = "";
         
-        this.spellList.addListSelectionListener(new ListSelectionListener() {
-            @Override
-            public void valueChanged(ListSelectionEvent e) {
-                Spell spell = (Spell) spellList.getSelectedValue();
-                try {
-                    selectedSpell = spell.getName();
-                    String tmp = "Name: " + selectedSpell + "\n";
-                    tmp += "Range: " + spell.getSpellRange() + "\n";
-                    tmp += "max Dmg: " + ( spell.getDamageDie() * spell.getDieCount() ) + "\n";
-                    spellInfoArea.setText(tmp);
-                } catch (Exception exception) {
-                    System.err.println("Ooops!!! => Lieste?");
+        this.initFrame();
+        this.addListModel();
+        this.addListListener();
+        this.addButtonListener();
+    }
+    
+    /**
+     * ToDo: play with the size etc, to make it look better.
+     */
+    private void initFrame()
+    {
+        this.setName("Spellbook");
+        this.setTitle(this.getName());
+        this.castSpellButton.setText("cast spell");
+
+        // setting the Size of the Frame
+        this.setSize(new Dimension(300, 300));
+        this.setResizable(false);
+        this.setFocusable(true);
+        
+        // Grid Layout of the Frame to put the components in.
+        this.setLayout(new GridLayout(2, 2));
+        this.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+        
+        // adding the comonents to the Frame
+        // the order predicts the Grid Position
+        this.add(this.scrollPane);
+        this.add(this.spellInfoArea);
+        this.add(this.castSpellButton);
+    }
+    
+    /**
+     * adding the data model to the list
+     */
+    private void addListModel()
+    {
+        DefaultListModel<Spell> liederListModel = new DefaultListModel<>();
+        for (Spell spell : Game.getPlayer().getpClass().getMyBook().getSpellBook()) {
+            liederListModel.addElement(spell);
+        }
+        this.spellList.setModel(liederListModel);
+        this.scrollPane.setViewportView(spellList);
+    }
+    
+    /**
+     * adding the selection listener to the list
+     */
+    private void addListListener(){
+        this.spellList.addListSelectionListener(
+            new ListSelectionListener(){
+                @Override
+                public void valueChanged(ListSelectionEvent e)
+                {
+                    Spell spell = (Spell) spellList.getSelectedValue();
+                    try
+                    {
+                        selectedSpell = spell.getName();
+                        String tmp = "Name: " + selectedSpell + "\n";
+                        tmp += "Range: " + spell.getSpellRange() + "\n";
+                        tmp += "max Dmg: " + ( spell.getDamageDie() * spell.getDieCount() ) + "\n";
+                        spellInfoArea.setText(tmp);
+                    }
+                    catch (Exception exception)
+                    {
+                        System.err.println("Ooops!!! => Lieste?");
+                    }
                 }
             }
-        });
-        
+        );
+    }
+    
+    /**
+     * adding the listener to the button
+     */
+    private void addButtonListener(){
         this.castSpellButton.addActionListener(
-            new java.awt.event.ActionListener()
-            {
+            new java.awt.event.ActionListener(){
                 @Override
                 public void actionPerformed(java.awt.event.ActionEvent evt)
                 {
@@ -60,39 +117,13 @@ public class SpellbookFrame extends JFrame
                         );
                         BattleHandler.tryToSpellAttack(attacker, target, selectedSpell);
                         Game.updateGUI();
-                    }else{
+                    }
+                    else
+                    {
                         System.err.println("Select a Spell to cast.");
                     }
                 }
             }
         );
-    }
-    
-    private void initList()
-    {
-        DefaultListModel<Spell> liederListModel = new DefaultListModel<>();
-        for (Spell spell : Game.getPlayer().getpClass().getMyBook().getSpellBook()) {
-            liederListModel.addElement(spell);
-        }
-        this.spellList.setModel(liederListModel);
-        this.scrollPane.setViewportView(spellList);
-        this.castSpellButton.setText("cast spell");
-        this.add(this.scrollPane);
-        this.add(this.spellInfoArea);
-        this.add(this.castSpellButton);
-    }
-    
-    private void configInternals()
-    {
-        Dimension d = new Dimension(400, 600);
-        this.scrollPane.setSize(d);
-        this.setSize(d);
-        this.setPreferredSize(d);
-        this.setMinimumSize(d);
-        this.setMaximumSize(d);
-        this.setResizable(false);
-        this.setFocusable(true);
-        this.setName("Spellbook");
-        this.setTitle(this.getName());
     }
 }

e06b7676cd5534ba3a5d9882ad8886e53d4db0f5
Matthias Dröge <Zelophobian@gmail.com>
2017-06-24 16:43:24 +0200

    added helpful comments

--- src/enums/Proficiencies.java
+++ src/enums/Proficiencies.java
@@ -5,27 +5,28 @@ package enums;
  * @author Matthias Dröge
  */
 public enum Proficiencies {
+//nothing
     NONE("none"),
-    
+//Armor Groups
     LIGHT_ARMOR("light armor"),
     MEDIUM_ARMOR("medium armor"),
     HEAVY_ARMOR("heavy armor"),
-    
+//shield group    
     SHIELDS("shields"),
-    
+//main weapon groups
     SIMPLE_WEAPONS("simple weapons"),
     MARTIAL_WEAPONS("martial weapons"),
-    
+// single melee weapons to list!
     QUATERSTAFF("quaterstaff"),
     DAGGER("dagger"),
     SHORTSWORD("shortsword"),
     LONGSWORD("longsword"),
-    
+// single range weapon to list!
     DARTS("darts"),
     SLING("sling"),
     LIGHT_CROSSBOW("light crossbow"),
     LONGBOW("longbow"),
-    
+//Stats names
     STRENGTH("strength"), 
     DEXTERITY("dexterityy"),
     CONSTITUTION("constitution"),
@@ -33,7 +34,7 @@ public enum Proficiencies {
     INTELLEGENCE("intellegence"),
     CHARISMA("charisma");
     
-    // ========================
+// ========================
     
     private String proficiency;
     private Proficiencies(String s){
